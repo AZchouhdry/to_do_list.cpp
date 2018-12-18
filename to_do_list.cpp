b13fd18b12f550f4267a463760ae249e5c29ecd8
Najeebahmed11 <Najeebahmed11>
2018-12-18 04:50:05 -0500

    added do while loop in the main which takes appointment description time and date from the user

---
+++ to_do_list.cpp
@@ -0,0 +1,19 @@
+#include <iostream>;
+using namespace std;
+int main()
+{
+int x,y=0;
+int arrey[10];
+
+do {
+  cout<<"press 1 or 2 "<<endl;
+  cin<<x;
+  if(x==1)
+  {string x;
+  cout<<"enter your today's task......"<<endl;
+  cin<<x;}
+  else if(x==2)
+  {cout<<arrey[i]<<endl;}
+} while(int y<10);
+return 0;
+}
